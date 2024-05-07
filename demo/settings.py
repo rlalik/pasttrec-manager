8ee6a6382eab3c144ab20120ac975ad1ea56a4fb
Rafał Lalik <rafal.lalik@uj.edu.pl>
2024-05-07 12:47:02 +0200

    Docs update

--- demo/settings.py
+++ demo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_pasttrec_manager.apps.PasttrecManager",
+    # extra apps
+    "django_pasttrec_manager",
     "formtools",
     "mathfilters",
     "django_bootstrap5",
